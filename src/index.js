23b0a15d2f7ca5772e0b0e651fd82a132906f6e6
Romana Reyna <romanareyna@ya.ru>
2021-01-22 09:40:25 +0300

    feat: implement toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const num = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 20) {
+        return num[number];
+    } else if (number < 100) {
+        let digit = number % 10;
+
+        return (
+            tens[Math.trunc(number / 10) - 2] + (digit ? " " + num[digit] : "")
+        );
+    } else {
+        return (
+            num[Math.trunc(number / 100)] +
+            " hundred" +
+            (number % 100 == 0 ? "" : " " + toReadable(number % 100))
+        );
+    }
+};
